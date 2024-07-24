0f1625887fe6b0b8b0d58f8f6e6b0b47be3c0681
Lokesh Rangineni <lokeshforjava@gmail.com>
2024-07-24 13:25:00 -0400

    Adding the test case for AuthenticatedRequestSession.
    Signed-off-by: Lokesh Rangineni <lokeshforjava@gmail.com>
    
    Signed-off-by: Lokesh Rangineni <lokeshforjava@gmail.com>

--- sdk/python/tests/unit/permissions/client/test_auth_session.py
+++ sdk/python/tests/unit/permissions/client/test_auth_session.py
@@ -1,10 +1,14 @@
-from feast.permissions.client.http_auth_requests_wrapper import AuthenticatedRequestsSession
+from feast.permissions.client.http_auth_requests_wrapper import (
+    AuthenticatedRequestsSession,
+)
 
 
 def test_authorization_header():
     token = "test_token"
     session = AuthenticatedRequestsSession(token)
-    assert "Authorization" in session.headers, "Authorization header not found in session headers"
-    assert session.headers["Authorization"] == f"Bearer {token}", "Authorization header value is incorrect"
-
-
+    assert (
+        "Authorization" in session.headers
+    ), "Authorization header not found in session headers"
+    assert (
+        session.headers["Authorization"] == f"Bearer {token}"
+    ), "Authorization header value is incorrect"
